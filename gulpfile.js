e3dee8324d38c86ef47a1cc753e0f6578bfa85c4
Rodrigo Noreña <Rodrigo Norea>
2016-08-03 14:56:33 -0500

    Added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -10,7 +11,7 @@ var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
-    'components/scripts/template.js'    
+    'components/scripts/template.js'
 ];
 var sassSources = [
     'components/sass/style.css'
@@ -27,11 +28,12 @@ gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js'));
+        .pipe(gulp.dest('builds/development/js'))
+        .pipe(connect.reload());
 })
 
 gulp.task('compass', function () {
-    gulp.src(sassSources)        
+    gulp.src(sassSources)
         .pipe(compass({
             sass: 'components/sass',
             image: 'builds/development/images',
@@ -39,7 +41,7 @@ gulp.task('compass', function () {
             css: 'builds/development/css'
         }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('./builds/development/css'));
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
@@ -48,5 +50,12 @@ gulp.task('watch', function () {
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('connect', function () {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    })
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
 
